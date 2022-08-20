b67fbe5c6564315262fc2efbdc728afdcd9131a6
smkambalekar <97361578+smkambalekar@users.noreply.github.com>
2022-08-20 19:30:53 +0530

    add code to reverselist

--- Reverselist.py
+++ Reverselist.py
@@ -1,1 +1,22 @@
-print("testing")
+import os
+
+list1 = [8, 9, 3, 6, 1, 10]
+list1.reverse()
+
+print("the reversed list is ", list1)
+
+list2 = [91, 67, 120, 34, 76, 54, 78, 87, 56, 64, 345]
+
+list2.sort()
+
+print("The sorted list is ", list2)
+
+list3 = []
+
+list3 = list1.copy()
+
+print("list3 =", list3)
+
+indexvalue = list2[2:6]
+
+print("The values are", indexvalue)
